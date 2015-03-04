cc5725c3d2f428cb0d7f3c7c2e6c652b2a613208
Daniel Barden <daniel.barden@gmail.com>
2015-03-04 22:05:28 +0100

    Give more breath room for the map

--- wormhole/Route.m
+++ wormhole/Route.m
@@ -77,8 +77,8 @@ static NSString *const TransportModesKey = @"transportModes";
     MKCoordinateRegion region;
     region.center.latitude = topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) * 0.5;
     region.center.longitude = topLeftCoord.longitude + (bottomRightCoord.longitude - topLeftCoord.longitude) * 0.5;
-    region.span.latitudeDelta = fabs(topLeftCoord.latitude - bottomRightCoord.latitude) * 1.1;
-    region.span.longitudeDelta = fabs(bottomRightCoord.longitude - topLeftCoord.longitude) * 1.1;
+    region.span.latitudeDelta = fabs(topLeftCoord.latitude - bottomRightCoord.latitude) * 1.4;
+    region.span.longitudeDelta = fabs(bottomRightCoord.longitude - topLeftCoord.longitude) * 1.4;
 
     return region;
 }
